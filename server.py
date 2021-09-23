7f300a10a9f3ee5d38ba9ac0c1a6af81e3faf4d8
Xueying Luo <snowvan113@gmail.com>
2021-09-22 23:02:20 -0600

    server completed

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 #  coding: utf-8 
-import socketserver
+import socketserver, os, requests
 
-# Copyright 2013 Abram Hindle, Eddie Antonio Santos
+
+# Copyright 2013 Abram Hindle, Eddie Antonio Santos, Xueying Luo
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -32,7 +33,90 @@ class MyWebServer(socketserver.BaseRequestHandler):
     def handle(self):
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+
+        # parse the get request 
+
+        # [0]=method(GET, etc) [1]=path, [6]=host (or [4]=host for browser/curl)
+        parsed_request = self.data.decode().split()  
+
+        host = "127.0.0.1:8080"
+        root = "./www"
+
+        # status code & headers 
+        status_200 = "HTTP/1.0 200 OK\r\n"
+        status_301 = "HTTP/1.0 301 Moved Permanently\r\n"
+        status_404 = "HTTP/1.0 404 Not Found\r\n"
+        status_405 = "HTTP/1.0 405 Method Not Allowed\r\n"
+        content_type_html = "Content-Type: text/html\r\n"
+        content_type_css = "Content-Type: text/css\r\n"
+        header_end = "\r\n"
+
+        if parsed_request[0] != 'GET':
+            # cannot handle methods other than GET - 405 Method Not Allowed
+            self.request.sendall(bytearray(status_405+header_end,'utf-8'))
+        
+        else: 
+
+            path = parsed_request[1]
+            if os.path.exists(root+path): # check whether path exists
+
+                if host == parsed_request[4] or host == parsed_request[6]:
+                    # correct host
+
+                    path_split = path.split('.')
+
+                    if path[-1] == '/':
+                        # display index.html
+
+                        self.request.sendall(bytearray(status_200+content_type_html+header_end,'utf-8'))
+
+                        file = open(root+path+"/index.html", "r")
+                        self.request.sendall(bytearray(file.read(), 'utf-8'))
+                        file.close()
+
+                    elif path_split[-1] == "html" :
+                        # display index.html
+
+                        self.request.sendall(bytearray(status_200+content_type_html+header_end,'utf-8'))
+
+                        file = open(root+path, "r")
+                        self.request.sendall(bytearray(file.read(), 'utf-8'))
+                        file.close()
+
+                    elif path_split[-1] == "css":
+                        # display css
+                        self.request.sendall(bytearray(status_200+content_type_css+header_end,'utf-8'))
+
+                        file = open(root+path, "r")
+                        self.request.sendall(bytearray(file.read(), 'utf-8'))
+                        file.close()
+
+                    elif path[-1] != '/' and not os.path.isfile(root+path):
+                        
+                        addr = root+ path + '/'
+
+                        if os.path.exists(addr):
+                            # incorret path - 301 Moved Permanently
+                            self.request.sendall(bytearray(status_301+"Location: "+host+path+'/'+header_end+header_end,'utf-8'))
+
+                        else:
+                            # 404 not found
+                            self.request.sendall(bytearray(status_404+header_end,'utf-8'))
+
+                    else:
+                        # wrong path - 404 Not Found
+                        self.request.sendall(bytearray(status_404+header_end,'utf-8'))
+
+
+            else:
+                # wrong path - 404 Not Found
+                self.request.sendall(bytearray(status_404+header_end,'utf-8'))
+
+
+        
+
+
+
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
@@ -44,3 +128,9 @@ if __name__ == "__main__":
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
     server.serve_forever()
+
+
+    
+
+
+
